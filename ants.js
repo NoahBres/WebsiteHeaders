35fb5ed50bce5121b3215a0a08c3b2b3901b6eb2
Noah <noahzgames@gmail.com>
2018-01-06 20:06:56 -0600

    🐜 Some new ants in your pants

--- ants.js
+++ ants.js
@@ -5,71 +5,95 @@ let branches = [];
 let mouseX = 0;
 let mouseY = 0;
 
-let tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
+let randomBranches = [];
+let randomX = 0;
+let randomY = 0;
+
+const timeBetweenChoosey = 3000;
+
+const tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
 
 class Branch {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.life = 500;
-        this.angle = Math.random() * 360;
-    }
-    
-    tick(ctx) {
-        this.life--;
-        
-        ctx.beginPath();
-        ctx.strokeStyle = "#87b5ff";
-        
-        ctx.moveTo(this.x, this.y);
-        
-        this.angle += Math.random() - .5;
-        this.x += Math.cos(this.angle);
-        this.y += Math.sin(this.angle);
-        
-        ctx.lineTo(this.x, this.y);
-        
-        ctx.stroke();
-        ctx.closePath();
-    }
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.life = 500;
+		this.angle = Math.random() * 360;
+	}
+	
+	tick(ctx) {
+		this.life--;
+		
+		ctx.beginPath();
+		ctx.strokeStyle = "#87b5ff";
+		
+		ctx.moveTo(this.x, this.y);
+		
+		this.angle += Math.random() - .5;
+		this.x += Math.cos(this.angle);
+		this.y += Math.sin(this.angle);
+		
+		ctx.lineTo(this.x, this.y);
+		
+		ctx.stroke();
+		ctx.closePath();
+	}
 }
 
 function loop() {
-    if(branches.length < 500) {
-        branches.push(new Branch(mouseX, mouseY));
-    }
-    
-    branches.forEach((b) => {
-        if(b.life <= 0) {
-            branches.shift();
-        }
-        
-        b.tick(ctx);
-    });
-    
-    ctx.fillStyle = "rgba(0, 0, 50, 0.1)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+	if(branches.length < 500)
+		branches.push(new Branch(mouseX, mouseY));
+	
+	if(randomBranches.length < 300)
+		randomBranches.push(new Branch(randomX, randomY));
+
+	branches.forEach((b) => {
+		if(b.life <= 0) {
+			branches.shift();
+		}
+		
+		b.tick(ctx);
+	});
+	
+	randomBranches.forEach((b) => {
+		if(b.life <= 0) {
+			randomBranches.shift();
+		}
+		
+		b.tick(ctx);
+	});
+
+	ctx.fillStyle = "rgba(0, 0, 50, 0.1)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function init() {
-    resizeCanvas();
-    
-    setInterval(loop, 1000 / 60);
+	resizeCanvas();
+	
+	chooseRandomAnts();
+	setInterval(loop, 1000 / 60);
 }
 
 function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 }
 
 function mouseMove(e) {
-    if(e.touches) {
-        e.preventDefault();
-        e = e.touches[0];
-    }
-    
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+	if(e.touches) {
+		e.preventDefault();
+		e = e.touches[0];
+	}
+	
+	mouseX = e.clientX;
+	mouseY = e.clientY;
+}
+
+function chooseRandomAnts() {
+	randomX = Math.random() * canvas.width;
+	randomY = Math.random() * canvas.height;
+
+	setTimeout(chooseRandomAnts, timeBetweenChoosey);
 }
 
 window.addEventListener('resize', resizeCanvas, false);
